6066c448582225d6978bf450672d222084df8125
SHUBHAM SINGH <shubham.singh267@gmail.com>
2022-02-15 11:32:57 +1300

    MovingFromOneFormtoAnother

--- app/src/main/java/com/example/navigation/MainActivity2.java
+++ app/src/main/java/com/example/navigation/MainActivity2.java
@@ -2,13 +2,27 @@ package com.example.navigation;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity2 extends AppCompatActivity {
 
+    private Button Move2;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main3);
+
+        Move2=findViewById(R.id.btnBAck);
+        Move2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                Intent intent= new Intent( MainActivity2.this,MainActivity.class );
+                startActivity(intent);
+            }
+        });
     }
 }
